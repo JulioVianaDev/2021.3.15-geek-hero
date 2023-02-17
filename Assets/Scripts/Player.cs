acd9a6522ed812ffc6bc8bd551034a579f9d345e
JulioVianaDev <JulioVianaDev@gmail.com>
2023-02-17 17:25:02 -0300

    andando ok

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,8 +8,6 @@ public class Player : MonoBehaviour
 
     public Rigidbody2D rig2D;
     
-    public float moveSpeed;
-    public Rigidbody2D rig2D;
     float moveX;
     float moveY;
     // Start is called before the first frame update
@@ -20,6 +18,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetAxisRaw("Vertical");
+        Move();
+    }
+
+    void Move()
+    {
+        /*Movimenta o personagem através da fisica do unity, utilizando nosso vetor de movimento
+    multiplicado por [Delta Time] para que a velocidade seja constante e independente
+    do frame rate do jogo e também multiplicando por [Move Speed] para que possamos escolher
+    a velocidade do personagem.
+    */
+
+        rig2D.MovePosition(transform.position + new Vector3(moveX, moveY, 0) * Time.deltaTime * moveSpeed);
     }
 }
